95e7495e5aa76071062f8f5b56fde5414befd639
Lau <laucapogabriel@gmail.com>
2024-10-22 23:32:09 -0300

    4th

--- Assets/Scripts/Base_Char.cs
+++ Assets/Scripts/Base_Char.cs
@@ -16,9 +16,13 @@ public class CharacterMovement : MonoBehaviour
     [SerializeField] float fallMultiplier;
     [SerializeField] float lowJumpMultiplier;
 
+    [SerializeField] private float _jumpBufferTime = 0.2f;
+    [SerializeField] private float _jumpBufferCounter;
+
     public Transform groundCheck;
     public Transform wallCheck;
     public LayerMask groundLayer;
+    public LayerMask wallLayer;
     Vector2 vecGravity;
 
     bool isJumping;
@@ -32,6 +36,7 @@ public class CharacterMovement : MonoBehaviour
     {
         vecGravity = new Vector2(0, -Physics2D.gravity.y);
         IsFacingRight = true;
+        float moveInput = Input.GetAxis("Horizontal");
         body = GetComponent<Rigidbody2D>();
         HorizontalMovement();
     }
@@ -42,31 +47,42 @@ public class CharacterMovement : MonoBehaviour
         body.velocity = new Vector2(moveInput * speed, body.velocity.y);
     }
 
+    
+
     public virtual void Jump()
     {
-        if (Input.GetKeyDown("space") && isGrounded())
+        if (Input.GetButtonDown("Jump"))
+        {
+            _jumpBufferCounter = _jumpBufferTime;
+        }
+
+        else
+        {
+            _jumpBufferCounter -= Time.deltaTime;
+        }
+
+        if (_jumpBufferCounter > 0f && isGrounded())
         {
             //body.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse); // un lance para arriba 
             body.velocity = new Vector2(body.velocity.x, jumpPower);
             isJumping = true; // marcar que esta saltando
             jumpCounter = 0; // tiempo de salto igual a serop
+            _jumpBufferCounter = 0f;           
+        }
 
-            if (body.velocity.y > 0 && isJumping)
-            {
-                jumpCounter += Time.deltaTime;
-
-            }
-
-
-
-
+        if (body.velocity.y > 0 && isJumping)
+        {
+            jumpCounter += Time.deltaTime;
             if (jumpCounter > jumpTime)
             {
                 isJumping = false; // Detener el salto cuando se supera el tiempo máximo de salto
+                if (isJumping == false)
+                {
+                    body.velocity -= vecGravity * fallMultiplier * Time.deltaTime;
+                }
 
             }
         }
-
         if (Input.GetButtonUp("Jump") && isJumping == true && body.velocity.y > 0)
         {
             isJumping = false;
@@ -136,11 +152,10 @@ public class CharacterMovement : MonoBehaviour
     }
     public virtual void WallSliding()
     {
-        // Detectar si está tocando la pared usando BoxCast para mayor precisión
-        bool isTouchingWall = Physics2D.BoxCast(transform.position, new Vector2(0.1f, 1f), 0f, Vector2.right * Mathf.Sign(moveInput), 0.1f, LayerMask.GetMask("Wall"));
+              
 
         // Si estás tocando una pared y hay movimiento hacia ella, reduce la velocidad de deslizamiento
-        if (isTouchingWall && moveInput != 0)
+        if (wallTouching() && moveInput != 0)
         {
             // Limitar la velocidad de caída al deslizarse por la pared
             body.velocity = new Vector2(body.velocity.x, Mathf.Clamp(body.velocity.y, -wallSlideSpeed, float.MaxValue));
@@ -149,7 +164,7 @@ public class CharacterMovement : MonoBehaviour
 
     bool wallTouching()
     {
-        return Physics2D.OverlapCapsule(wallCheck.position, new Vector2(1.8f, 0.3f), CapsuleDirection2D.Horizontal, 0, groundLayer);
+        return Physics2D.OverlapCapsule(wallCheck.position, new Vector2(1.8f, 0.3f), CapsuleDirection2D.Horizontal, 0, wallLayer);
     }
 
     void Update()
