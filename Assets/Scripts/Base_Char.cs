4bf5eaa53219124927af235b14fc46a879c6c6f0
Lau <laucapogabriel@gmail.com>
2024-11-05 22:13:57 -0300

    5th commit
    Jump Wall grab and WallJump

--- Assets/Scripts/Base_Char.cs
+++ Assets/Scripts/Base_Char.cs
@@ -7,17 +7,17 @@ public class CharacterMovement : MonoBehaviour
 {
     protected float speed = 5f;
     protected float wallSlideSpeed = 0.9f;  // Velocidad de deslizamiento en paredes
-    float moveInput = Input.GetAxis("Horizontal");
-    [SerializeField] float lerpAmount = 3f;
+    protected float moveInput = Input.GetAxis("Horizontal");
+    [SerializeField] protected float lerpAmount = 3f;
     bool IsFacingRight;
-    [SerializeField] int jumpMultiplier;
-    [SerializeField] int jumpPower;
-    [SerializeField] float jumpTime;
-    [SerializeField] float fallMultiplier;
-    [SerializeField] float lowJumpMultiplier;
+    [SerializeField] protected int jumpMultiplier;
+    [SerializeField] protected float jumpPower;
+    [SerializeField] protected float jumpTime;
+    [SerializeField] protected float fallMultiplier;
+    [SerializeField] protected float lowJumpMultiplier;
 
-    [SerializeField] private float _jumpBufferTime = 0.2f;
-    [SerializeField] private float _jumpBufferCounter;
+    [SerializeField] protected float _jumpBufferTime = 0.2f;
+    [SerializeField] protected float _jumpBufferCounter;
 
     public Transform groundCheck;
     public Transform wallCheck;
@@ -27,12 +27,12 @@ public class CharacterMovement : MonoBehaviour
 
     bool isJumping;
     float jumpCounter;
-
-
-    Rigidbody2D body;
+     
+    
+    protected Rigidbody2D body;
     bool isTouchingWall;
 
-    private void Start()
+    protected virtual void Start()
     {
         vecGravity = new Vector2(0, -Physics2D.gravity.y);
         IsFacingRight = true;
@@ -45,12 +45,14 @@ public class CharacterMovement : MonoBehaviour
         float moveInput = Input.GetAxis("Horizontal");
         speed = Mathf.Lerp(body.velocity.y, lerpAmount, speed);
         body.velocity = new Vector2(moveInput * speed, body.velocity.y);
-    }
+
+           }
 
     
 
     public virtual void Jump()
     {
+      
         if (Input.GetButtonDown("Jump"))
         {
             _jumpBufferCounter = _jumpBufferTime;
@@ -63,8 +65,8 @@ public class CharacterMovement : MonoBehaviour
 
         if (_jumpBufferCounter > 0f && isGrounded())
         {
-            //body.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse); // un lance para arriba 
-            body.velocity = new Vector2(body.velocity.x, jumpPower);
+            body.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse); // un lance para arriba 
+            
             isJumping = true; // marcar que esta saltando
             jumpCounter = 0; // tiempo de salto igual a serop
             _jumpBufferCounter = 0f;           
@@ -82,12 +84,13 @@ public class CharacterMovement : MonoBehaviour
                 }
 
             }
+            if (Input.GetButtonUp("Jump"))
+            {
+                isJumping = false;
+                body.velocity = new Vector2(body.velocity.x, body.velocity.y * lowJumpMultiplier);
+            }
         }
-        if (Input.GetButtonUp("Jump") && isJumping == true && body.velocity.y > 0)
-        {
-            isJumping = false;
-            body.velocity = new Vector2(body.velocity.x, body.velocity.y * lowJumpMultiplier);
-        }
+        
         
         if (body.velocity.y < 0 || isJumping == false)
         {
@@ -95,83 +98,20 @@ public class CharacterMovement : MonoBehaviour
         }
         
         
-        
-
-        // Si el personaje está cayendo
-
     } 
-    // Al saltar
-    /*   if (Input.GetKeyDown("space") && isGrounded())
-       {
-           body.velocity = new Vector2(body.velocity.x, jumpPower); // Asigna directamente la velocidad para evitar acumulación
-           isJumping = true;
-           jumpCounter = 0;
-
-           if (body.velocity.y > 0 && isJumping)
-           {
-               jumpCounter += Time.deltaTime;
-               if (jumpCounter > jumpTime)
-               {
-                   isJumping = false; // Detener el salto cuando se supera el tiempo máximo de salto
-               }
-           }
-
-       }
-       // Mientras el jugador está subiendo y manteniendo el botón de salto
-       if (body.velocity.y > 0 && isJumping)
-       {
-           jumpCounter += Time.deltaTime;
-           if (jumpCounter > jumpTime)
-           {
-               isJumping = false; // Detener el salto cuando se supera el tiempo máximo de salto
-           }
-       }
-
-
-    // Si suelta el botón de salto antes de llegar a la altura máxima
-    if (Input.GetButtonUp("Jump"))
+    public bool isGrounded()
     {
-        isJumping = false;
-        if (body.velocity.y > 0)
-        {
-            // Aplica un multiplicador de caída rápida si se suelta el botón de salto
-            body.velocity += vecGravity * lowJumpMultiplier * Time.deltaTime;
-        }
+        return Physics2D.OverlapCapsule(groundCheck.position, new Vector2(0.6f, 0.01f), CapsuleDirection2D.Vertical, 0, groundLayer);
     }
+   
 
-    // Si el personaje está cayendo
-    if (body.velocity.y < 0)
-    {
-        // Aplica un multiplicador mayor al caer para hacerlo más rápido
-        body.velocity -= vecGravity * fallMultiplier * Time.deltaTime;
-    }
-} */
-    bool isGrounded()
-    {
-        return Physics2D.OverlapCapsule(groundCheck.position, new Vector2(1.8f, 0.3f), CapsuleDirection2D.Horizontal, 0, groundLayer);
-    }
-    public virtual void WallSliding()
-    {
-              
+   
 
-        // Si estás tocando una pared y hay movimiento hacia ella, reduce la velocidad de deslizamiento
-        if (wallTouching() && moveInput != 0)
-        {
-            // Limitar la velocidad de caída al deslizarse por la pared
-            body.velocity = new Vector2(body.velocity.x, Mathf.Clamp(body.velocity.y, -wallSlideSpeed, float.MaxValue));
-        }
-    }
-
-    bool wallTouching()
-    {
-        return Physics2D.OverlapCapsule(wallCheck.position, new Vector2(1.8f, 0.3f), CapsuleDirection2D.Horizontal, 0, wallLayer);
-    }
-
-    void Update()
+    protected virtual void Update()
     {
         Jump();
              HorizontalMovement();
-        WallSliding();
+        
     }
 }
 
--- Assets/Scripts/Girl_Char.cs
+++ Assets/Scripts/Girl_Char.cs
@@ -2,17 +2,86 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GirlMovement : CharacterMovement
-{
-    // Start is called before the first frame update
-    void Start()
+    public class GirlMovement : CharacterMovement
     {
+    [SerializeField] private float wallRaycastDistance = 0.9f;
+    
+    private bool isGrabbingWall = false;
+    public bool hitRightWall;
+    public bool hitLefttWall;    
+    public bool canWallJump;
+    public float wallgrabBufferTime = 0.1f;
+    public float wallgrabBufferCounter;
+        protected override void Start()
+        {
+            base.Start();  // Llama a la lógica Start() de CharacterMovement
+                           // Lógica adicional específica de GirlMovement, si es necesario
+        }
+    protected override void Update()
+    {
+        base.Update();
+        WallGrab();
+        OnDrawGizmos();
+        
+    }
+
+
+    private void WallGrab()
+    {
+        hitRightWall = Physics2D.Raycast(transform.position, Vector2.right, wallRaycastDistance, groundLayer);
+        hitLefttWall = Physics2D.Raycast(transform.position, Vector2.left, wallRaycastDistance, groundLayer);
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            wallgrabBufferCounter = wallgrabBufferTime;
+        }
+
+        else
+        {   
+            wallgrabBufferCounter -= Time.deltaTime;
+        }  
+
+        if (hitLefttWall == true || hitRightWall == true && wallgrabBufferCounter > 0f)
+        {
+            isGrabbingWall = true;
+            body.constraints = RigidbodyConstraints2D.FreezePosition;
+            canWallJump = true;
+            wallgrabBufferCounter = 0f;
+            if (Input.GetButtonDown("Jump"))
+            {
+                WallJump();
+            }
+        }
+        else if (Input.GetKeyUp(KeyCode.Z))
+        {
+            isGrabbingWall = false;
+            body.gravityScale = 2;
+            body.constraints = RigidbodyConstraints2D.None;
+            body.constraints = RigidbodyConstraints2D.FreezeRotation;
+            canWallJump = false;
+        }
+        // Si hay una pared a la izquierda o derecha y el jugador presiona "Z", se agarra
 
     }
 
-        
-    void Update()
+    private void WallJump()
     {
-
+        isGrabbingWall = false;
+        body.constraints = RigidbodyConstraints2D.None;
+        body.constraints = RigidbodyConstraints2D.FreezeRotation;
+        if (hitLefttWall == true)
+        {
+            body.AddForce(Vector2.right * jumpPower, ForceMode2D.Impulse);
+        }
+        if (hitRightWall == true)
+        {
+            body.AddForce(Vector2.left * jumpPower, ForceMode2D.Impulse);
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.left * wallRaycastDistance);
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.right * wallRaycastDistance);
     }
 }
