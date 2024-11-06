d7877a6e01966aaa99c880b5ad0f9d4636403817
Guidoparisi91 <guidoparisi91@gmail.com>
2024-11-06 20:59:51 -0300

    6th commit

---
+++ Assets/Scripts/CharacterSwitch.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSwitch : MonoBehaviour
+{
+    public GirlMovement girlControler;
+    public CatMovement catControler;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
